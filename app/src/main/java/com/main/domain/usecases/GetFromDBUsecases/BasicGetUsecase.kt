fb6c639c71a06303ac445f69677b4bcec99c78b8
Vladzaharinsky <zahar.vlad23@gmail.com>
2024-07-10 10:06:13 +0200

    fixed problem with mappers

--- app/src/main/java/com/main/domain/usecases/GetFromDBUsecases/BasicGetUsecase.kt
+++
@@ -1,7 +0,0 @@
-package com.main.domain.usecases.GetFromDBUsecases
-
-abstract class BasicGetUsecase <out Type, in Params> where Type: Any? {
-    abstract fun execute (params: Params) : Type
-}
-
-object None
